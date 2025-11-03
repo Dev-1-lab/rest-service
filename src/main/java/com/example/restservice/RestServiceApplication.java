863c97c99903a260f552ae8b4ca56926d4875935
javadev <bestactor301@gmail.com>
2025-11-03 10:36:20 +0500

    Committing changes

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,7 +2,6 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sun.misc.Unsafe;
 
 import java.util.concurrent.atomic.AtomicLong;
 
